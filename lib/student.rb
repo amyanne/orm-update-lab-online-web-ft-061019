7ce7b44a1b43599dd8ce7534fae7964a8aed31b2
Amy Westcott <amy.westcott@yahoo.com>
2019-07-29 15:58:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,12 +63,15 @@ class Student
   end
   
   def self.find_by_name
-    def self.new_from_db(row)
-    new_student = self.new 
-    new_student.id = row[0]
-    new_student.name =  row[1]
-    new_student.grade = row[2]
-    new_student
-  end
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+ 
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end 
 end
