ee2edeb8e42b88a38ba8a0fd4312b3ecc18ed8e8
Amy Westcott <amy.westcott@yahoo.com>
2019-07-29 16:00:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,15 +66,7 @@ class Student
   end
   
   def self.find_by_name
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
-    SQL
+    
+  end
  
-    DB[:conn].execute(sql, name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end 
 end
